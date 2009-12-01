9fced676e27990db5ac300d901b883ece85037e5
migue030 <migue030@1b38764a-db03-11de-a9c8-216ecde6309e>
2009-12-01 10:36:34 +0000

    order by score

--- soccer_pool/app/controllers/results_controller.rb
+++ soccer_pool/app/controllers/results_controller.rb
@@ -1,6 +1,28 @@
 class ResultsController < ApplicationController
 	
-  def show_results
-  end
+ def show_results
+  
+  #Query para sacar columna de user
+  soccer_users = User.all(:select => "id,login,score",:conditions => "login <> 'admin'",:order =>"score desc")
+  
+	#Queries para sacar columnas de pools
+	@pool_concluded = Pool.all(:select => "id,name",:conditions => "status = 'concluded'")
+	
+	@ren_user = Array.new
+	
+	soccer_users.each do |user|
+		col_pool = Array.new
+		sum = 0
+		col_pool << user.login
+		@pool_concluded.each do |pool|
+			temp = Result.all(:conditions => ["user_id = :user_id and pool_id = :pool_id",{:user_id => user.id,:pool_id => pool.id}]).size
+			col_pool << temp
+			sum += temp
+		end
+		col_pool << sum
+		@ren_user << col_pool
+	end
+
+ end
 
 end
